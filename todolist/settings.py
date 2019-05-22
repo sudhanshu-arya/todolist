ae6466b193b6c6c98ec68bf3539e8888a1dbbd17
sudhanshu-arya <sudhanshuarya1199@gmail.com>
2019-05-22 22:18:56 +0530

    Initial commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,9 +25,11 @@ SECRET_KEY = '_vvxjhocf8pa(rjawv09jwy8=feqvz^z^q8(1h7fucu&*8_yi!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['todolist000.herokuapp.com']
 
 LOGIN_URL="/login/"
+
+LOGIN_REDIRECT_URL='/'
 
 # Application definition
 
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,12 +120,19 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static')
-]
-
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL='/'
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
